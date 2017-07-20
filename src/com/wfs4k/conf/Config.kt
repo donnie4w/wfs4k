e1742f0848f584a71162397f0fb94920af5ad697
donnie <donnie4w@gmail.com>
2017-07-20 18:04:09 +0800

    wfs4k

--- src/com/wfs4k/conf/Config.kt
+++ src/com/wfs4k/conf/Config.kt
@@ -1,5 +1,7 @@
 package com.wfs4k.conf
 
+import com.sun.org.apache.xpath.internal.operations.Bool
+
 /**
  * @Author donnie {donnie4w@gmail.com}
  * @Date 2017/7/6
@@ -7,7 +9,7 @@ package com.wfs4k.conf
 
 class Config {
     companion object {
-        val version  = "1.0.0"
+        val version = "1.0.0"
         var port: Int = 3434
         var chiplimit: Int = 0
         var maxDataSize: Long = 1 shl 30
@@ -20,8 +22,37 @@ class Config {
         var bind: String = "0.0.0.0"
         var compress: Boolean = false
 
-        fun parse(){
+        private val _p = "-p"
+        private val _MD = "-MD"
+        private val _max = "-max"
+        private var _fileData = "-fileData"
+        private var _readonly = "-readonly"
+        private var _keepalive = "-keepalive"
+        private var _rps = "-rps"
+        private var _srt = "-srt"
+        private var _bind = "-bind"
+        private var _compress = "-compress"
 
+
+        fun parse(args: Array<String>): Boolean {
+            try {
+                if (args != null && args.size > 0) {
+                    println(args.size)
+                    for (i in 0..args.size) {
+                        when {
+                            args[i] == _p -> port = args[i + 1].toInt()
+                            args[i] == _MD -> maxDataSize = args[i + 1].toLong()
+                            args[i] == _max -> maxFileSize = args[i + 1].toLong()
+                            args[i] == _fileData -> fileData = args[i + 1]
+                            args[i] == _readonly -> readonly = true
+                            args[i] == _bind -> bind = args[i]
+                        }
+                    }
+                }
+            } catch (e: Exception) {
+                return false
+            }
+            return true
         }
     }
 
